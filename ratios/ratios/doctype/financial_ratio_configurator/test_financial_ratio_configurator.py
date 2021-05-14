34f9d118870de2fb62a03319f369f3aab0375d55
Tunde Akinyanmi <tundebabzy@gmail.com>
2021-05-14 14:30:43 +0100

    start by adding Financial Ratio Configurator

---
+++ ratios/ratios/doctype/financial_ratio_configurator/test_financial_ratio_configurator.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, Babatunde Akinyanmi and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestFinancialRatioConfigurator(unittest.TestCase):
+	pass
---
+++ ratios/ratios/doctype/financial_ratio_sales_account/financial_ratio_sales_account.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, Babatunde Akinyanmi and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class FinancialRatioSalesAccount(Document):
+	pass
